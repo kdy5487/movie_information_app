2b06226914216ca3165160f92b324179310d9200
kdy5487 <kdykdy5487@naver.com>
2025-05-14 23:56:16 +0900

    api연동

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
-  static String get tmdbToken => dotenv.env['TMDB_BEARER_TOKEN'] ?? '';
+  static String get tmdbToken => dotenv.env['TMDB_API_KEY'] ?? '';
 }
--- test/api_test.dart
+++ test/api_test.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
